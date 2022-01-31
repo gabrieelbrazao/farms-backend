8455f460ec706eb7ec3398624b19d1a9dce3a6e9
Gabriel Brazão <gabriel.b@esquadrilon.londrina>
2022-01-31 16:12:01 -0300

    add controller farms.create

--- app/Controllers/Http/FarmsController.ts
+++ app/Controllers/Http/FarmsController.ts
@@ -1,9 +1,25 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import Farm from 'App/Models/Farm'
 
 export default class FarmsController {
   public async index() {
     return await Farm.query().preload('state').preload('cultures')
+  }
+
+  public async create({ request, response }: HttpContextContract) {
+    const data = { ...request.body() }
+    delete data.cultures
+
+    const farm = await Farm.create(data)
+
+    if (!farm.$isPersisted) {
+      response.internalServerError({ erro: 'Erro ao tentar criar fazenda' })
+      return
+    }
+
+    farm.related('cultures').attach(request.body().cultures)
+
+    response.noContent()
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -2,4 +2,5 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/', 'FarmsController.index')
+  Route.post('/', 'FarmsController.create')
 }).prefix('/farms')
