39aa13e2ebe0bd7a14a78e6faefa814bc270b436
Gabriel Brazão <gabriel.b@esquadrilon.londrina>
2022-01-31 16:29:58 -0300

    add controller farms.update

--- app/Controllers/Http/FarmsController.ts
+++ app/Controllers/Http/FarmsController.ts
@@ -3,8 +3,9 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Farm from 'App/Models/Farm'
 
 export default class FarmsController {
-  public async index() {
-    return await Farm.query().preload('state').preload('cultures')
+  public async index({ response }: HttpContextContract) {
+    const farms = await Farm.query().preload('state').preload('cultures')
+    response.ok(farms)
   }
 
   public async create({ request, response }: HttpContextContract) {
@@ -19,6 +20,29 @@ export default class FarmsController {
     }
 
     farm.related('cultures').attach(request.body().cultures)
+
+    response.noContent()
+  }
+
+  public async update({ request, response }: HttpContextContract) {
+    const farm = await Farm.find(request.param('id'))
+
+    if (!farm) {
+      response.notFound({ erro: 'Fazenda não encontrada' })
+      return
+    }
+
+    const data = { ...request.body() }
+    delete data.cultures
+
+    await farm.merge(data).save()
+
+    if (!farm.$isPersisted) {
+      response.internalServerError({ erro: 'Erro ao tentar atualizar fazenda' })
+      return
+    }
+
+    await farm.related('cultures').sync(request.body().cultures)
 
     response.noContent()
   }
--- start/routes.ts
+++ start/routes.ts
@@ -3,4 +3,5 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.get('/', 'FarmsController.index')
   Route.post('/', 'FarmsController.create')
+  Route.put('/:id', 'FarmsController.update')
 }).prefix('/farms')
